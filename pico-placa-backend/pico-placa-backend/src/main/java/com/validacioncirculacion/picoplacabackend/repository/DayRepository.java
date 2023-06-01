9089f1087263ffb9c264b571f17011969fe307d5
Leslymarisol <lesly.tipanluiza@epn.edu.ec>
2023-05-31 19:10:11 -0500

    some changes

--- pico-placa-backend/pico-placa-backend/src/main/java/com/validacioncirculacion/picoplacabackend/repository/DayRepository.java
+++ pico-placa-backend/pico-placa-backend/src/main/java/com/validacioncirculacion/picoplacabackend/repository/DayRepository.java
@@ -11,16 +11,6 @@ import com.validacioncirculacion.picoplacabackend.entity.Day;
 
 @Repository
 public interface DayRepository extends JpaRepository<Day, Long>{
-    List<Day> findByWeekdayContaining(String weekday);
- 
-
-    @Query(value=("SELECT d FROM Day d WHERE d.idDay LIKE %:filtro%"))
-    List<Day> search(@Param("filtro") Integer filtro);
-
-    @Query(
-        value = "SELECT * FROM day WHERE day.weekday LIKE %:filtro%",
-        nativeQuery = true
-    )
-    List<Day> searchNative(@Param("filtro") String filtro);
+    
    
 }
